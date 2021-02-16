27d8a2c7b2369e53e320e9e84cf9d1c6c23c339c
zidol <ajh3166@naver.com>
2021-02-17 00:39:31 +0900

    quert dsl join on

--- src/test/java/study/querydsl/QueryBasicTest.java
+++ src/test/java/study/querydsl/QueryBasicTest.java
@@ -247,4 +247,41 @@ public class QueryBasicTest {
                 .extracting("username")
                 .containsExactly("teamA", "teamB");
     }
+
+    /**
+     *  에) 회원과 팀을 조인하면서, 팀 이름이 teamA인 조인, 회원은 모두 조회
+     *  JPAL : select m, t from Member m left join m.team t on t.name = 'teamA'
+     */
+    @Test
+    public void join_on_filtering() {
+        List<Tuple> result = queryFactory
+                .select(member, team)
+                .from(member)
+                .join(member.team, team).on(team.name.eq("teamA"))
+                .fetch();
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    /**
+     * 연관관계가 없는 엔티티 외부 조인
+     * 회원의 이름이 팀 이름과 같은 회원 외부 조회
+     */
+    @Test
+    public void join_on_no_relation() {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        em.persist(new Member("teamC"));
+
+        List<Tuple> results = queryFactory
+                .select(member, team)
+                .from(member)
+                .leftJoin(team).on(member.username.eq(team.name))
+                .where(member.username.eq(team.name))
+                .fetch();
+        for (Tuple result : results) {
+            System.out.println("result = " + result);
+        }
+    }
 }
