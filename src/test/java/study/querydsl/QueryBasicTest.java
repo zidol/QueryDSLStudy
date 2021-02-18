8204e9987d49b9794c5f53f930df97be8b4452c0
zidol <ajh3166@naver.com>
2021-02-18 23:53:44 +0900

    query dsl projection return value

--- src/test/java/study/querydsl/QueryBasicTest.java
+++ src/test/java/study/querydsl/QueryBasicTest.java
@@ -440,4 +440,29 @@ public class QueryBasicTest {
             System.out.println("s = " + s);
         }
     }
+    
+    @Test
+    public void simpleProjection() {
+        List<String> fetch = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+        for (String s : fetch) {
+            System.out.println("s = " + s);
+        }
+    }
+    
+    @Test
+    public void tubplePorjection() {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
 }
